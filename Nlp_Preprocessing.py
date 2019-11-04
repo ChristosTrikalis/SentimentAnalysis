f7a45e6a5c05d24045c24d417ebc6f077854a5dc
TrikalisChristos <43909927+ChristosTrikalis@users.noreply.github.com>
2019-11-04 20:06:45 +0200

    Add files via upload

--- Nlp_Preprocessing.py
+++ Nlp_Preprocessing.py
@@ -1,47 +1,32 @@
-import operator
-
-import nltk
 import pandas as pd
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import json
-import os
-import collections
 
 def get_data():
-
     with open("C:\\Users\\Trika\\Desktop\\ScrapyProjectExample\\tutorial\\example.json") as json_file:
         data = json.load(json_file)
     print(len(data)/2)
     dict_to_dataframe(data)
 
-
 def dict_to_dataframe(data):
-    dfTitles = pd.DataFrame()
-    dfLinks = pd.DataFrame()
-    title_keys = []
-    link_keys = []
+    titles_and_links = pd.DataFrame()
     titles = []
     links = []
     length = (len(data)/2).__int__()
+
     for i in range(0,length):
-        dictTemp = data[i]
-        for k,v in dictTemp.items():
-            title_keys.append(k)
+        dictTemp1 = data[i]
+        dictTemp2 = data[i+length]
+        for k,v in dictTemp1.items():
             titles.append(v)
-    for i in range(length,len(data)):
-        dictTemp = data[i]
-        for k, v in dictTemp.items():
-            link_keys.append(k)
+        for k,v in dictTemp2.items():
             links.append(v)
 
-    dfTitles['numOftitle'] = title_keys
-    dfTitles['titles'] = titles
-    dfLinks['numOfLink'] = link_keys
-    dfLinks['links'] = links
-    print(dfTitles)
-    print(dfLinks)
-    dfTitles['titles'] = stopwords_removal(dfTitles['titles'])
+    titles_and_links['titles'] = titles                                        #fills first column with titles
+    titles_and_links['links'] = links                                          #fills second column with links
+    print(titles_and_links)                                                  #debugging
+    titles_and_links['titles'] = stopwords_removal(titles_and_links['titles'])
 
 def stopwords_removal(data):
     stop_words = set(stopwords.words('english'))
@@ -55,6 +40,4 @@ def stopwords_removal(data):
                 filtered_sentence.append(w)
         sentences.append(filtered_sentence)
 
-
-    print(sentences)
 get_data()
