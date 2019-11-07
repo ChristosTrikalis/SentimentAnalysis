0a8172c7512cdcafcdb5865ee6ddf765182118f0
TrikalisChristos <43909927+ChristosTrikalis@users.noreply.github.com>
2019-11-07 20:22:07 +0200

    Add files via upload

--- Nlp_Preprocessing.py
+++ Nlp_Preprocessing.py
@@ -6,7 +6,6 @@ import json
 def get_data():
     with open("C:\\Users\\Trika\\Desktop\\ScrapyProjectExample\\tutorial\\example.json") as json_file:
         data = json.load(json_file)
-    print(len(data)/2)
     dict_to_dataframe(data)
 
 def dict_to_dataframe(data):
@@ -25,19 +24,23 @@ def dict_to_dataframe(data):
 
     titles_and_links['titles'] = titles                                        #fills first column with titles
     titles_and_links['links'] = links                                          #fills second column with links
-    print(titles_and_links)                                                  #debugging
-    titles_and_links['titles'] = stopwords_removal(titles_and_links['titles'])
 
-def stopwords_removal(data):
+    print(titles_and_links.iloc[1,0])                                                    #debugging before text normalization
+    titles_and_links['titles'] = stopwords_removal_and_canonicalization(titles_and_links['titles'])
+    print("Canonicalization and stop words removal: \n")
+    print(titles_and_links.iloc[1,0])                                                    #debugging after text normalization
+
+def stopwords_removal_and_canonicalization(data):
     stop_words = set(stopwords.words('english'))
     sentences = []
-
+    import string
     for i in data:
-        word_tokens=word_tokenize(i)
+        result = i.translate(str.maketrans('', '', string.punctuation))
+        word_tokens=word_tokenize(result)
         filtered_sentence = []
         for w in word_tokens:
             if w not in stop_words:
                 filtered_sentence.append(w)
         sentences.append(filtered_sentence)
-
+    return sentences
 get_data()
